01ffdb4ecbc4ac7a6d0da301babc93b53270a83b
Gavldac <75696834+Gavldac@users.noreply.github.com>
2024-10-29 22:26:14 -0600

    Update App.java
    Clean up of the test client due to an old print statement not getting removed earlier.

--- App.java
+++ App.java
@@ -15,9 +15,7 @@ public class App {
     }
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-
-        /*
+         /*
          * Input: nums1 = [1,2,3,0,0,0], m = 3, 
          *        nums2 = [2,5,6], n = 3
          * Output: [1,2,2,3,5,6]
